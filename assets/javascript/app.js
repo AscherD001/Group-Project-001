5b052872c071b56163944d21e8cf99dc01972454
jbcurrie <biobacker30@gmail.com>
2017-07-25 09:27:46 -0400

    updated index and assets with the presentation copy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -461,11 +461,11 @@ function cycleImg(index) {
 }
 function cseSearch(query) {
 	// var cseKey = "AIzaSyBQWDimnA-AjyNZlXIsh_R3Ld8wYlAksfA";
-	var cseKey = "AIzaSyDrufMCRtOuOdYgbTXT-piKR3A-hZb5YvU";
+	var cseKey = "AIzaSyBI_c00PTMT_3qV5IWFAf2dw-hShmxT9qU";
 	// var cseKey = "AIzaSyD_SVLOZ-31PIlN_XCdKJAYXlw5yHFvcUw"
 	var SEid = "004303949972187002826:5vg83odxtam";
 	// var query = prompt("Enter a Search");
-	var queryURL = "https://www.googleapis.com/customsearch/v1?&key=" + cseKey + "&searchType=image&imgSize=large&cx=" + SEid + "&q=" + query + "&excludeTerms=hotel|food|entertainment|logo|brand|home|dreams|journalist|auto|car|map|selfie|default" + "&safe=high";
+	var queryURL = "https://www.googleapis.com/customsearch/v1?&key=" + cseKey + "&searchType=image&imgSize=large&cx=" + SEid + "&q=" + query +"+skyline" + "&excludeTerms=hotel|food|entertainment|logo|brand|home|dreams|journalist|auto|car|map|selfie|default" + "&safe=high";
 	$.get(queryURL, function(data) {
 		cseData = data;
 		// console.log(cseData);
